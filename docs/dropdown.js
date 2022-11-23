cfc814bf07aad43bc1172fbe7a4fb78548b561b6
ches <112760526+ches41@users.noreply.github.com>
2022-11-23 23:19:36 +0800

    add new site (gs)
    added a new sub site and changed the structure of the entire site, so some things might be broken in the fgc site

--- docs/dropdown.js
+++
@@ -1,6 +0,0 @@
-
-
-function toggleDropdown() {
-    document.getElementById("dropdown-content").classList.toggle("show-dropdown");
-    document.getElementById("dropdown-button").classList.toggle("rotate-dropdown-button");
-}
---
+++ docs/fgc/dropdown.js
@@ -0,0 +1,6 @@
+
+
+function toggleDropdown() {
+    document.getElementById("dropdown-content").classList.toggle("show-dropdown");
+    document.getElementById("dropdown-button").classList.toggle("rotate-dropdown-button");
+}
